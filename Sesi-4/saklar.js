1e812d5c0451e5268806d4da6afbcb596a325f49
RestuMuhamadAkbar09 <150891730+RestuMuhamadAkbar09@users.noreply.github.com>
2023-12-04 14:45:28 +0700

    Add files via upload

---
+++ Sesi-4/saklar.js
@@ -0,0 +1,83 @@
+function saklar() {
+    let toggle1 = document.getElementById("default-toggle1");
+    let toggle2 = document.getElementById("default-toggle2");
+    let toggle3 = document.getElementById("default-toggle3");
+    let toggle4 = document.getElementById("default-toggle4");
+    let toggle5 = document.getElementById("default-toggle5");
+    let toggle6 = document.getElementById("default-toggle6");
+    let toggle7 = document.getElementById("default-toggle7");
+    let toggle8 = document.getElementById("default-toggle8");
+    let toggle9 = document.getElementById("default-toggle9");
+    let toggle10 = document.getElementById("default-toggle10");
+
+    if (toggle1.checked) {
+        // Lampu 1 Nyala
+        lampu1.src = "assets/image/on.gif";
+    } else {
+        // Lampu 1 Mati
+        lampu1.src = "assets/image/off.gif";
+    }
+    if (toggle2.checked) {
+        // Lampu 2 Nyala
+        lampu2.src = "assets/image/on.gif";
+    } else {
+        // Lampu 2 Mati
+        lampu2.src = "assets/image/off.gif";
+    }
+    if (toggle3.checked) {
+        // Lampu 3 Nyala
+        lampu3.src = "assets/image/on.gif";
+    } else {
+        // Lampu 3 Mati
+        lampu3.src = "assets/image/off.gif";
+    }
+    if (toggle4.checked) {
+        // Lampu 4 Nyala
+        lampu4.src = "assets/image/on.gif";
+    } else {
+        // Lampu 4 Mati
+        lampu4.src = "assets/image/off.gif";
+    }
+    if (toggle5.checked) {
+        // Lampu 3 Nyala
+        lampu5.src = "assets/image/on.gif";
+    } else {
+        // Lampu 5 Mati
+        lampu5.src = "assets/image/off.gif";
+    }
+    if (toggle6.checked) {
+        // Lampu 6 Nyala
+        lampu6.src = "assets/image/on.gif";
+    } else {
+        // Lampu 6 Mati
+        lampu6.src = "assets/image/off.gif";
+    }
+    if (toggle7.checked) {
+        // Lampu 7 Nyala
+        lampu7.src = "assets/image/on.gif";
+    } else {
+        // Lampu 7 Mati
+        lampu7.src = "assets/image/off.gif";
+    }
+    if (toggle8.checked) {
+        // Lampu 8 Nyala
+        lampu8.src = "assets/image/on.gif";
+    } else {
+        // Lampu 8 Mati
+        lampu8.src = "assets/image/off.gif";
+    }
+    if (toggle9.checked) {
+        // Lampu 9 Nyala
+        lampu9.src = "assets/image/on.gif";
+    } else {
+        // Lampu 9 Mati
+        lampu9.src = "assets/image/off.gif";
+    }
+    if (toggle10.checked) {
+        // Lampu 10 Nyala
+        lampu10.src = "assets/image/on.gif";
+    } else {
+        // Lampu 10 Mati
+        lampu10.src = "assets/image/off.gif";
+    }
+}
